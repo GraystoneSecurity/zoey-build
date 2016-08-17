ff3e64979b9780eb5bdf316373b2cd0a9744cc1a
dgraystone <dgraystone@graystonesec.com>
2016-08-17 11:21:01 -0500

    build scripts - WIP

--- base/build.sh
+++ base/build.sh
@@ -6,7 +6,7 @@ sudo mkdir /opt/zoey/modules
 sudo yum clean all
 sudo yum update -y
 
-echo Assumes python 2.7.6 already installed. Checking python version
+echo Assumes python 2.7.5 or greater already installed. Checking python version
 if which python > /dev/null 2>&1;
 then
     #Python is installed
@@ -30,16 +30,18 @@ sudo yum install python-pip
 
 ## GitPython (https://github.com/gitpython-developers/GitPython)
 sudo mkdir /opt/zoey/modules/GitPython
-git clone git@github.com:gitpython-developers/GitPython.git
-git submodule update --init --recursive
-cd /opt/zoey/modules/GitPython
-sudo python setup.py install
+# install process needs work after this, can't authenticate to GitHub to pull. Pubkey error
+#git clone git@github.com:gitpython-developers/GitPython.git
+#git submodule update --init --recursive
+#cd /opt/zoey/modules/GitPython
+#sudo python setup.py install
 
 ## NLTK 
 # Natural Language Toolkit (https://github.com/nltk/nltk)
 sudo mkdir /opt/zoey/modules/NLTK
-git clone git@github.com:nltk/nltk.git
-git submodule update --init --recursive
-cd /opt/zoey/modules/NLTK
-sudo python setup.py install
+# install process needs work after this, can't authenticate to GitHub to pull. Pubkey error
+#git clone git@github.com:nltk/nltk.git
+#git submodule update --init --recursivecd 
+#cd /opt/zoey/modules/NLTK
+#sudo python setup.py install
 
