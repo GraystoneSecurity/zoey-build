e9312079166172d25f7ef8f9ab85be94e57d9b47
Daniel Graystone <daniel@graystone.solutions>
2017-01-18 21:03:13 -0600

    Version 0.0.4
    Signed-off-by: Daniel Graystone <daniel@graystone.solutions>

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Graystone Solutions: Project Zoey
 # 
-## 
-Project codename "Zoey" is a discovery project intended to discover the scope and possibly of building a rudimentary A.I. "being" capable of self-learning and self-improvement. Graystone understands that many other project such as this exist (https://en.wikipedia.org/wiki/List_of_artificial_intelligence_projects) our attempt is not to surpase these projects, but merely to understand this endevor and self-improve our own knowledge in this growing field.
-## Under The Hood
+## Philosophy
+Project codename "Zoey" is an exploratory effort to discover the scope and scalabilty of the current private A.I. landscape without the backing of a major congolomorate and teams of developers. Graystone understands that many other project such as this exist (https://en.wikipedia.org/wiki/List_of_artificial_intelligence_projects) our attempt is not to surpase these projects, but merely to understand this endevor and cultivate our own knowledge in this bleeding edge horizon.
+## Foundation
+Zoey is written in the python language and developed to operate on a Centos 7.0 platform using a basic static. Zoey brings her own dependency stack to the host and installs accordingly. A basic Zoey install will operate using the Natural Language Toolkit for rudimenatry linguistics.
 
-Zoey is written in the python language and developed in Centos 7.0. Zoey comes pre-packaged with several features and can take advantage of new APIs, each of which can be installed as a seperate modules. For module installation, see MODULES.txt
+## Evolution
+Future plans include several code modules which can be installed for interface with various platforms such as social media networks, or smart home systems.
 
 
-## For More On Project Zoey
-https://graystone.solutions/project_zoey
+For more information see: [Project Zoey](https://graystone.solutions/project_zoey)
---
+++ build.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+import os
+import shutil
+import stat
+import subprocess
+
+from setuptools import setup
+
+
+def read(fname):
+    return open (os.path.join (os.path.dirname (__file__), fname)).read ()
+
+
+setup (
+    name="Zoey Graystone",
+    version="0.0.4",
+    author="Daniel Graystone",
+    author_email="daniel@graystone.solutions",
+    description=("An interactive A.I."),
+    license="M.I.T.",
+    keywords="Zoey Graystone A.I.",
+    url="https://graystone.solutions/projectzoey",
+    packages=['Zoey Graystone', 'tests'],
+    long_description=read ('README.md'),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.7"
+    ],
+)
+buildroot = "/home/zoey/Projects/zoey-build/"
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    if not os.path.exists (dst):
+        os.makedirs (dst)
+        shutil.copystat (src, dst)
+    lst = os.listdir (src)
+    if ignore:
+        excl = ignore (src, lst)
+        lst = [x for x in lst if x not in excl]
+    for item in lst:
+        s = os.path.join (src, item)
+        d = os.path.join (dst, item)
+        if symlinks and os.path.islink (s):
+            if os.path.lexists (d):
+                os.remove (d)
+            os.symlink (os.readlink (s), d)
+            try:
+                st = os.lstat (s)
+                mode = stat.S_IMODE (st.st_mode)
+                os.lchmod (d, mode)
+            except:
+                pass  # lchmod not available
+        elif os.path.isdir (s):
+            copytree (s, d, symlinks, ignore)
+        else:
+            shutil.copy2 (s, d)
+
+
+# GitPython (https://github.com/gitpython-developers/GitPython)
+def gitpython_install():
+    gitphyloaddir = '/home/zoey/Projects/zoey-build/module_installer/GitPython'
+    gitpyopdir = '/opt/zoey_graystone/modules/GitPython'
+    os.makedirs (gitphyloaddir, 0755)
+    subprocess.call ("git clone git@github.com:gitpython-developers/GitPython.git")
+    # Submodule updates to be run from top level down
+    os.chdir (buildroot)
+    subprocess.call ("git submodule update --init --recursive")
+    # Move Files into place
+    os.makedirs (gitpyopdir, 0755)
+
+    def copytree():
+        s = (gitphyloaddir)
+        d = (gitpyopdir)
+
+    # Run Setup
+    os.chdir (gitpyopdir)
+    os.system ("setup.py install")
+    subprocess.call ("./init-tests-after-clone.sh")
+
+
+# Natural Language Toolkit (https://github.com/nltk/nltk)
+def nltk_install():
+    nltkloaddir = '/home/zoey/Projects/zoey-build/module_installer/nltk'
+    nltkopdir = '/opt/zoey_graystone/modules/nltk'
+    os.makedirs (nltkloaddir, 0755)
+    subprocess.call ("git clone git@github.com:nltk/nltk.git")
+    # Submodule updates to be run from top level down
+    os.chdir (buildroot)
+    subprocess.call ("git submodule update --init --recursive")
+    # Move Files into place
+    os.makedirs (nltkopdir, 0755)
+
+    def copytree():
+        s = (nltkloaddir)
+        d = (nltkopdir)
+
+    # Run Setup
+    os.chdir (nltkopdir)
+    os.system ("setup.py install")
